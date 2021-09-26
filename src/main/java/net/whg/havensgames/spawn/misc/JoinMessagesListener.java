318c79bdab8b5610a0f680f146ca6c8a11268d76
TheDudeFromCI <thedudefromci@gmail.com>
2021-09-26 01:53:39 -0700

    Added quit messages

--- src/main/java/net/whg/havensgames/spawn/misc/JoinMessagesListener.java
+++ src/main/java/net/whg/havensgames/spawn/misc/JoinMessagesListener.java
@@ -1,19 +1,19 @@
 package net.whg.havensgames.spawn.misc;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-import me.clip.placeholderapi.PlaceholderAPI;
 import net.whg.utils.WraithLib;
 
 /**
  * Handles sending out the player join messages, configurable via config.
  */
 public class JoinMessagesListener implements Listener {
-    private final String messageToJoiner;
-    private final String messageToEveryoneElse;
+    private final FileConfiguration config;
 
     /**
      * Creates a new JoinMessagesListener object and loads the configured join
@@ -21,15 +21,13 @@ public class JoinMessagesListener implements Listener {
      */
     public JoinMessagesListener() {
         var plugin = Bukkit.getPluginManager().getPlugin("HavensGames");
-        var config = plugin.getConfig();
+        config = plugin.getConfig();
 
-        config.addDefault("JoinMessages.ToPlayer", "Welcome to the server!");
-        config.addDefault("JoinMessages.ToEveryoneElse", "{} has joined the server!");
+        config.addDefault("JoinMessages.ToPlayer", "Welcome to the server, %player_name%!");
+        config.addDefault("JoinMessages.ToEveryoneElse", "%player_name% has joined the server!");
+        config.addDefault("QuitMessages.ToEveryoneElse", "%player_name% has left the server!");
         config.options().copyDefaults(true);
         plugin.saveConfig();
-
-        messageToJoiner = config.getString("JoinMessages.ToPlayer");
-        messageToEveryoneElse = config.getString("JoinMessages.ToEveryoneElse");
     }
 
     /**
@@ -42,14 +40,39 @@ public class JoinMessagesListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent e) {
         e.joinMessage(null);
 
+        var messageToJoiner = config.getString("JoinMessages.ToPlayer");
+        var messageToEveryoneElse = config.getString("JoinMessages.ToEveryoneElse");
+
         var player = e.getPlayer();
-        WraithLib.log.sendMessage(player, PlaceholderAPI.setPlaceholders(player, messageToJoiner));
+
+        WraithLib.log.sendPlaceholderMessage(player, player, messageToJoiner);
 
         for (var p : Bukkit.getOnlinePlayers()) {
             if (p == player)
                 continue;
 
-            WraithLib.log.sendMessage(player, PlaceholderAPI.setPlaceholders(p, messageToEveryoneElse));
+            WraithLib.log.sendPlaceholderMessage(p, player, messageToEveryoneElse);
+        }
+    }
+
+    /**
+     * Listens for player quit events and then overrides the quit message to send
+     * the corresponding messages that were assigned from the config file.
+     * 
+     * @param e - The event.
+     */
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        e.quitMessage(null);
+
+        var messageToEveryoneElse = config.getString("QuitMessages.ToEveryoneElse");
+
+        var player = e.getPlayer();
+        for (var p : Bukkit.getOnlinePlayers()) {
+            if (p == player)
+                continue;
+
+            WraithLib.log.sendPlaceholderMessage(p, player, messageToEveryoneElse);
         }
     }
 }
